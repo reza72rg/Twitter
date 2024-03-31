be2359ca02b7d0199cbf8d26c4985eef070a752c
reza.latifi <reza72rg@gmail.com>
2024-03-31 16:56:45 +0330

    class base view
    in this commit I create first class base view for my posts with APIView

--- core/twitter/api/v1/urls.py
+++ core/twitter/api/v1/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from .views import (
     PostListApi,
     PostApiDelete,
+    PostListAPIView,
+    PostDetailAPIView,
 )
 
 
 urlpatterns = [
     path("task/",PostListApi,name="task-list"),
     path("task/<int:pk>/",PostApiDelete,name="task-delete"),
+    path("task/cbv/",PostListAPIView.as_view(),name="task-list_cbv"),
+    path("task/cbv/<int:pk>/",PostDetailAPIView.as_view(),name="task-list_cbv"),
 ]
--- core/twitter/api/v1/views.py
+++ core/twitter/api/v1/views.py
@@ -3,10 +3,57 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.request import Request
+from rest_framework.views import APIView
 from twitter.models import Post
 from .serializers import PostSerializers
+
 # Create your views here.
 
+# region class base views
+
+class PostListAPIView(APIView):
+    def get(self, request: Request):
+        posts = Post.objects.all()
+        posts_serializer = PostSerializers(posts, many=True)
+        return Response(posts_serializer.data, status.HTTP_200_OK)
+    
+    def post(self, request: Request):
+        serializer= PostSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(None, status.HTTP_400_BAD_REQUEST)
+    
+class PostDetailAPIView(APIView):
+    def get_objects(self, pk:int):
+        try:
+            post = Post.objects.get(pk = pk)
+            return post
+        except Post.DoesNotExist:
+            return Response(None, status.HTTP_404_NOT_FOUND)
+        
+    def get(self, request: Request, pk:int):
+        post = self.get_objects(pk)
+        posts_serializer = PostSerializers(post)
+        return Response(posts_serializer.data, status.HTTP_200_OK)
+    
+    def put(self, request: Request, pk:int):
+        post = self.get_objects(pk)
+        serializer = PostSerializers(post, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status.HTTP_201_CREATED)
+        return Response(None, status.HTTP_400_BAD_REQUEST)
+    def delete(self, request: Request, pk:int):
+        post = self.get_objects(pk)
+        post.delete()
+        return Response(None, status.HTTP_202_ACCEPTED)
+        
+# endregion
+
+
+
+# region function base views
 
 @api_view(['GET','POST'])
 def PostListApi(request: Request):
@@ -45,5 +92,5 @@ def PostApiDelete(request : Request, pk: int):
         post.delete()
         return Response(None, status.HTTP_204_NO_CONTENT)
 
-        
+# endregion      
     
