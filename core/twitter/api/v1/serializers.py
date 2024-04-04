c0e92c70e968ce945bee7f506f1193e43d5cefbb
reza.latifi <reza72rg@gmail.com>
2024-04-04 16:11:54 +0330

    author read_only
    In this commit I change my author to read_only_fields to get user in request

--- core/twitter/api/v1/serializers.py
+++ core/twitter/api/v1/serializers.py
@@ -12,17 +12,31 @@ class PostSerializers(serializers.ModelSerializer):
     snippet = serializers.CharField( source = 'get_snippet', read_only= True)
     relative_url = serializers.URLField( source= 'get_absolute_url', read_only= True)
     absolute_url = serializers.SerializerMethodField()
-    author = serializers.SlugRelatedField(
-        many=False, slug_field="user.username", queryset=Profile.objects.all()
-    )
+    # author = serializers.SlugRelatedField(
+    #     many=False, slug_field="user.username", queryset=Profile.objects.all()
+    # )
     
     class Meta:
         model = Post
         fields = ['id', 'content','author', 'snippet','relative_url', 'absolute_url', 'archive']
+        read_only_fields = ['author']
     def get_absolute_url(self, obj):
         request = self.context.get('request')
         return request.build_absolute_uri(obj.pk)
-
+    
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        rep = super().to_representation(instance)
+        if request.parser_context.get('kwargs').get('pk'):
+            rep.pop('snippet')
+            rep.pop('absolute_url')
+            rep.pop('relative_url')
+        else:
+            rep.pop('content')
+        return rep
+    def create(self, validated_data):
+        validated_data['author'] = Profile.objects.get(user__id = self.context.get('request').user.id)
+        return super().create(validated_data)
     
 class UserSerializers(serializers.ModelSerializer):
     posts_author = PostSerializers(read_only= True, many= True)
