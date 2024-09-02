211634094739287e183c9aaa7e6dc8cbe05856af
reza72rg <reza72rg@gmail.com>
2024-09-02 16:38:14 +0330

    I add custommiddleware.py to redirect to login page.

---
+++ core/core/middleware.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect
+from rest_framework.exceptions import NotAuthenticated
+
+
+class LoginRequiredMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        if isinstance(response, NotAuthenticated):
+            return redirect('/login/')  # Redirect to your login URL
+        return response
