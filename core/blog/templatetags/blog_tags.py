f83860c16327ddac90dd82816538f2c5715c82ae
reza72rg <reza72rg@gmail.com>
2024-09-02 17:41:19 +0330

    I add templatetags for count comments.

---
+++ core/blog/templatetags/blog_tags.py
@@ -0,0 +1,10 @@
+from django import template
+
+from comment.models import Comment
+
+register = template.Library()
+
+
+@register.simple_tag(name='comments_count')
+def function(pid):
+    return Comment.objects.filter(post=pid, approach=True).count()
