ca6e5b83c9eae22a9a60f18d0a5736b6e67cf42f
reza72rg <reza72rg@gmail.com>
2024-09-03 14:41:04 +0330

    I write insert_data for test databases.

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -1,11 +1,13 @@
 from django.core.management.base import BaseCommand
 from faker import Faker
-from accounts.models import User, Profile
-from blog.models import Category, Post
+from accounts.models import User, Profile, Follow
+from blog.models import Category, Post, Like, DisLike
+
+from comment.models import Comment
 
 
 class Command(BaseCommand):
-    help = "Insert dump data into User, Profile, Category, and Post models"
+    help = "Insert dump data into User, Profile, Category, Post, Like, DisLike, and Comment models"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -13,47 +15,67 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         # Create users and profiles
+        users = []
         for _ in range(5):
             username = self.fake.user_name()
             email = self.fake.email()
             password = "123456789ab"
 
-            # Create a user
             user = User.objects.create_user(username=username, email=email, password=password)
-
-            # Create a profile for the user
             profile, created = Profile.objects.get_or_create(user=user)
             profile.first_name = self.fake.first_name()
             profile.last_name = self.fake.last_name()
             profile.description = self.fake.paragraph(nb_sentences=5)
             profile.save()
 
+            users.append(profile)
             self.stdout.write(self.style.SUCCESS(f"Successfully created user with username: {username}"))
 
         # Create categories
-        for _ in range(5):
-            Category.objects.create(name=self.fake.word())
+        categories = [Category.objects.create(name=self.fake.word()) for _ in range(5)]
 
-        # Get all created categories and profiles
-        categories = list(Category.objects.all())
-        profiles = list(Profile.objects.all())
-
-        if not categories:
-            self.stdout.write(self.style.ERROR('No categories found. Please create some categories first.'))
-            return
-
-        if not profiles:
-            self.stdout.write(self.style.ERROR('No profiles found. Please create some profiles first.'))
-            return
-
-        # Create posts with random content and a random category
+        # Create posts
+        posts = []
         for _ in range(5):
             category = self.fake.random_element(elements=categories)
-            profile = self.fake.random_element(elements=profiles)
-            Post.objects.create(
+            profile = self.fake.random_element(elements=users)
+            post = Post.objects.create(
                 author=profile,
                 content=self.fake.paragraph(nb_sentences=5),
                 category=category
             )
+            posts.append(post)
 
         self.stdout.write(self.style.SUCCESS('Successfully created 5 posts with random data.'))
+
+        # Create likes, dislikes, and comments
+        for _ in range(5):
+            profile = self.fake.random_element(elements=users)
+            profile_follow = self.fake.random_element(elements=users)
+            post = self.fake.random_element(elements=posts)
+
+            # Create like
+            Like.objects.create(
+                user=profile,
+                post=post
+            )
+
+            # Create dislike
+            DisLike.objects.create(
+                user=profile,
+                post=post
+            )
+
+            # Create comment
+            Comment.objects.create(
+                author=profile,
+                post=post,
+                content=self.fake.paragraph(nb_sentences=1)
+            )
+            # Create Follow
+            Follow.objects.create(
+                user=profile,
+                follow_user=profile_follow,
+            )
+
+        self.stdout.write(self.style.SUCCESS('Successfully created likes, dislikes, Follow, and comments.'))
